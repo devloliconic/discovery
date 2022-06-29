9bc413eceb3a201fc6f3929ad1cccbb27895161f
devloliconic <kirillevsmirnov@gmail.com>
2022-06-30 06:20:47 +0700

    add some scripts

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import '../scss/main.scss';
 
 
 
+
 // import Swiper and modules styles
 
 
@@ -18,3 +19,19 @@ const swiper = new Swiper('.swiper', {
   modules: [Navigation, Pagination]
   
 });
+
+
+
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
