c2e4c487d0ff2fc1b02ed8b30eb6772a481d92ef
devloliconic <kirillevsmirnov@gmail.com>
2022-06-29 10:13:58 +0700

    add swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+//import 'swiper/css/pagination';
 
 import 'normalize.css';
 
@@ -5,5 +9,12 @@ import '../scss/main.scss';
 
 
 
+// import Swiper and modules styles
 
 
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  
+});
